22de3390b9383c7308ae0440741728b3bf4c33ce
Tax0787 <93974004+Tax0787@users.noreply.github.com>
2023-06-23 22:02:31 +0900

    help... mirror

--- MyTermux.sh
+++ MyTermux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir TaxTermuxSetting
 cd TaxTermuxSetting
+echo "#!/bin/sh\ntermux-change-repo" > TryPkgErrorFixByTax0787.sh
 echo "#!/bin/sh\ncd storage/shared/Terminal/MyGitHelp\nsh TaxAliasTip.sh\npython3 TaxGitSet.py\nbash" > MyGitDir.sh
 echo "#!/bin/sh\nmv MyGitDir.sh ../MyGitDir.sh\nmv TaxTermuxSet1.sh ../TaxTermuxSet1.sh\nmv TaxTermuxSet2.sh ../TaxTermuxSet2.sh\nmv TaxTermuxSet3.sh ../TaxTermuxSet3.sh" > install.sh
 echo "#!/bin/sh\npkg update && pkg upgrade -y\ntermux-setup-storage\ncd /\ntermux-setup-storage\nexit" > TaxTermuxSet1.sh
