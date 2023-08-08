9cdb4a3d55681ea0a797a2a1c3bbf09c6a61dc5b
Tax0787 <in_huan08@naver.com>
2023-08-08 19:45:48 +0900

    wow...umm

--- UseReplitCli_html_make.py
+++ UseReplitCli_html_make.py
@@ -1,1 +1,8 @@
-pass
+from MyHtml.MakeHtml import *
+
+ThisFilesSource = t()
+
+ThisFilesSource = source + ThisFilesSource
+
+with open(__file__.replace('_html_make.py', '.html'), 'w') as f:
+    f.write(ThisFilesSource)
